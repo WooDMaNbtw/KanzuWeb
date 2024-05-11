f1de89d0d7b8a95215645ae08170f17677b05f30
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-05-11 20:27:47 +0400

    Основной файл

---
+++ kanzueng-web/src/pages/main.jsx
@@ -0,0 +1,14 @@
+import NavBar from '../components/navbar/navbar';
+import Landing from "../components/landing/landing";
+
+function Main() {
+    return (
+        <div>
+            <NavBar/>
+            <Landing/>
+        </div>
+    )
+}
+
+
+export default Main;
