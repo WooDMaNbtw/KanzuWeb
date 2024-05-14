34236ce8619304c78444d8d0efdbbb9237206fa2
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-05-14 11:29:57 +0400

    added WhatWeDo component

---
+++ kanzueng-web/src/components/landing/WhatWeDo/landingWWD.jsx
@@ -0,0 +1,22 @@
+import Kanzu from "../../../assets/Kanzu1.png";
+
+const LandingWhatWeDo = () => {
+    return (
+        <>
+            <section id="what-we-do" className="section">
+                {/*<div className="content">*/}
+                {/*    <div className="image-container">*/}
+                {/*        <img src={Kanzu} alt="Portfolio"/>*/}
+                {/*        <div className="overlay">*/}
+                {/*            <h2>What We Do</h2>*/}
+                {/*            <p>Proin varius imperdiet mauris, vel fermentum lorem ultricies ac. In ac nisi varius,*/}
+                {/*                feugiat enim sit amet, convallis nunc.</p>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
+            </section>
+        </>
+    )
+}
+
+export default LandingWhatWeDo
--- kanzueng-web/src/components/landing/landing.jsx
+++ kanzueng-web/src/components/landing/landing.jsx
@@ -1,50 +1,52 @@
 import './landing.css'; // Подключаем стили
 import Kanzu from "../../assets/Kanzu1.png"
 import LandingFirstPage from "./FirstPage/landingFirstPage";
+import LandingWhatWeDo from "./WhatWeDo/landingWWD";
 
 const Landing = () => {
     return (
         <div>
             <LandingFirstPage/>
+            <LandingWhatWeDo/>
 
-            <section id="portfolio" className="section">
-                <div className="content">
-                    <div className="image-container">
-                        <img src={Kanzu} alt="Portfolio"/>
-                        <div className="overlay">
-                            <h2>Portfolio</h2>
-                            <p>Proin varius imperdiet mauris, vel fermentum lorem ultricies ac. In ac nisi varius,
-                                feugiat enim sit amet, convallis nunc.</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            {/*<section id="portfolio" className="section">*/}
+            {/*    <div className="content">*/}
+            {/*        <div className="image-container">*/}
+            {/*            <img src={Kanzu} alt="Portfolio"/>*/}
+            {/*            <div className="overlay">*/}
+            {/*                <h2>Portfolio</h2>*/}
+            {/*                <p>Proin varius imperdiet mauris, vel fermentum lorem ultricies ac. In ac nisi varius,*/}
+            {/*                    feugiat enim sit amet, convallis nunc.</p>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
 
-            <section id="feedback" className="section">
-                <div className="content">
-                    <div className="image-container">
-                        <img src={Kanzu} alt="Feedback"/>
-                        <div className="overlay">
-                            <h2>Feedback</h2>
-                            <p>Integer id ipsum nec libero tincidunt semper. Aliquam erat volutpat. Sed ac enim nec sem
-                                consectetur viverra.</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            {/*<section id="feedback" className="section">*/}
+            {/*    <div className="content">*/}
+            {/*        <div className="image-container">*/}
+            {/*            <img src={Kanzu} alt="Feedback"/>*/}
+            {/*            <div className="overlay">*/}
+            {/*                <h2>Feedback</h2>*/}
+            {/*                <p>Integer id ipsum nec libero tincidunt semper. Aliquam erat volutpat. Sed ac enim nec sem*/}
+            {/*                    consectetur viverra.</p>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
 
-            <section id="contact" className="section">
-                <div className="content">
-                    <div className="image-container">
-                        <img src={Kanzu} alt="Contact"/>
-                        <div className="overlay">
-                            <h2>Contact</h2>
-                            <p>Maecenas consequat, odio ac malesuada pharetra, justo risus dignissim justo, ut
-                                vestibulum magna lacus id justo.</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            {/*<section id="contact" className="section">*/}
+            {/*    <div className="content">*/}
+            {/*        <div className="image-container">*/}
+            {/*            <img src={Kanzu} alt="Contact"/>*/}
+            {/*            <div className="overlay">*/}
+            {/*                <h2>Contact</h2>*/}
+            {/*                <p>Maecenas consequat, odio ac malesuada pharetra, justo risus dignissim justo, ut*/}
+            {/*                    vestibulum magna lacus id justo.</p>*/}
+            {/*            </div>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
 
             {/*<div className="landing-content">*/}
             {/*    /!* Первая картинка *!/*/}
