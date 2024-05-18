737b0c69e2800e774b18f4b78827c19483956471
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-05-19 02:23:38 +0400

    added blank commponent wwd

--- kanzueng-web/src/components/landing/WhatWeDo/landingWWD.jsx
+++ kanzueng-web/src/components/landing/WhatWeDo/landingWWD.jsx
@@ -4,16 +4,16 @@ const LandingWhatWeDo = () => {
     return (
         <>
             <section id="what-we-do" className="section">
-                {/*<div className="content">*/}
-                {/*    <div className="image-container">*/}
-                {/*        <img src={Kanzu} alt="Portfolio"/>*/}
-                {/*        <div className="overlay">*/}
-                {/*            <h2>What We Do</h2>*/}
-                {/*            <p>Proin varius imperdiet mauris, vel fermentum lorem ultricies ac. In ac nisi varius,*/}
-                {/*                feugiat enim sit amet, convallis nunc.</p>*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+                <div className="content">
+                    <div className="image-container">
+                        <img src={Kanzu} alt="Portfolio"/>
+                        <div className="overlay">
+                            <h2>What We Do</h2>
+                            <p>Proin varius imperdiet mauris, vel fermentum lorem ultricies ac. In ac nisi varius,
+                                feugiat enim sit amet, convallis nunc.</p>
+                        </div>
+                    </div>
+                </div>
             </section>
         </>
     )
