62168cec5dfdc3740080a484c792c5fe0d703d7f
WooDMaNbtw <nikita.profatilov@gmail.com>
2024-05-26 00:45:53 +0400

    Added new finished component WhatWeDo main block

--- kanzueng-web/src/components/landing/WhatWeDo/landingWWD.jsx
+++ kanzueng-web/src/components/landing/WhatWeDo/landingWWD.jsx
@@ -1,22 +1,111 @@
-import Kanzu from "../../../assets/Kanzu1.png";
+import "./landingWWD.css"
+import radarChart from "../../../assets/img.png"
+import CardWWDComponentLeft from "./cards";
+import CardWWDComponentRight from "./cards";
+import {useEffect, useState} from "react";
+
+
+const panels = ['left-top', 'left-bottom', 'right-top', 'right-bottom'];
 
 const LandingWhatWeDo = () => {
+    const [activePanel, setActivePanel] = useState('left-top');
+    const [isHovered, setIsHovered] = useState(false);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (!isHovered) {
+                setActivePanel((prevPanel) => {
+                    const currentIndex = panels.indexOf(prevPanel);
+                    const nextIndex = (currentIndex + 1) % panels.length;
+                    return panels[nextIndex];
+                });
+            }
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, [isHovered]);
+
+    const handleMouseEnter = () => setIsHovered(true);
+    const handleMouseLeave = () => setIsHovered(false);
+
+    const getH2Style = (panel) => {
+        return panel === activePanel
+            ? { color: 'black', fontWeight: 'bold', transform: 'scale(1.08)', transition: '.5s ease-in-out' }
+            : { color: 'gray', transition: '.5s ease-in-out' };
+    };
+
+    const isCardVisible = (panel) => {
+        return panel === activePanel;
+    };
+
     return (
-        <>
-            <section id="what-we-do" className="section">
-                <div className="content">
-                    <div className="image-container">
-                        <img src={Kanzu} alt="Portfolio"/>
-                        <div className="overlay">
-                            <h2>What We Do</h2>
-                            <p>Proin varius imperdiet mauris, vel fermentum lorem ultricies ac. In ac nisi varius,
-                                feugiat enim sit amet, convallis nunc.</p>
-                        </div>
+        <section id="what-we-do" className="section section--wwd">
+            <h2 className="wwd-header">what we do</h2>
+            <div className="content">
+                <div
+                    className={`left-panel ${activePanel.startsWith('left') ? 'visible visible-left' : ''}`}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <div className="top-panel">
+                        <h2 style={getH2Style('left-top')}>chat-bot development</h2>
+                        {isCardVisible('left-top') && (
+                            <CardWWDComponentLeft
+                                flex={"flex-end"}
+                                tags={['bot', 'telegram', 'js']}
+                                descText={"We’re pro experts at developing chat-bots for your business. Automize processes and make your customer service next level with chat-bots"}
+                                buttonText={"develop chat-bot"}
+                            />
+                        )}
+                    </div>
+                    <div className="bottom-panel">
+                        {isCardVisible('left-bottom') && (
+                            <CardWWDComponentLeft
+                                flex="flex-end"
+                                tags={['bot', 'telegram', 'js']}
+                                descText={"We’re pro experts at developing chat-bots for your business. Automize processes and make your customer service next level with chat-bots"}
+                                buttonText={"develop chat-bot"}
+                            />
+                        )}
+                        <h2 style={getH2Style('left-bottom')}>System administration</h2>
                     </div>
                 </div>
-            </section>
-        </>
-    )
-}
+                <div className="center-panel">
+                    <div className="radar-chart">
+                        <img src={radarChart} alt="Radar Chart" className="radar-chart-image"/>
+                    </div>
+                </div>
+                <div
+                    className={`right-panel ${activePanel.startsWith('right') ? 'visible visible-right' : ''}`}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <div className="top-panel">
+                        <h2 style={getH2Style('right-top')}>Websites & web projects</h2>
+                        {isCardVisible('right-top') && (
+                            <CardWWDComponentRight
+                                flex="flex-start"
+                                tags={['bot', 'telegram', 'js']}
+                                descText={"We’re pro experts at developing chat-bots for your business. Automize processes and make your customer service next level with chat-bots"}
+                                buttonText={"develop chat-bot"}
+                            />
+                        )}
+                    </div>
+                    <div className="bottom-panel">
+                        {isCardVisible('right-bottom') && (
+                            <CardWWDComponentRight
+                                flex="flex-start"
+                                tags={['bot', 'telegram', 'js']}
+                                descText={"We’re pro experts at developing chat-bots for your business. Automize processes and make your customer service next level with chat-bots"}
+                                buttonText={"develop chat-bot"}
+                            />
+                        )}
+                        <h2 style={getH2Style('right-bottom')}>Mobile development</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
 
-export default LandingWhatWeDo
+export default LandingWhatWeDo;
